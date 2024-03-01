73cd92bce7397de0e3d2fbcff840f765206e6b28
lucasoliveiraa <lucasjackcosta18@gmail.com>
2024-02-29 23:34:48 -0300

    🚧 chore: create category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -2,13 +2,20 @@ import { Body, Controller, Post } from '@nestjs/common'
 import { Roles } from 'src/decorators/roles.decorator'
 import { UserId } from 'src/decorators/user-id.decorator'
 import { UserType } from 'src/user/enum/user-type.enum'
+import { CategoryService } from './category.service'
+import { CreateCategory } from './dto/create-category.dto'
+import { CategoryEntity } from './entities/category.entity'
 
 @Controller('category')
-@Roles(UserType.User)
+@Roles(UserType.Admin, UserType.Root, UserType.User)
 export class CategoryController {
-  @Roles(UserType.User)
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Roles(UserType.Admin, UserType.Root, UserType.User)
   @Post()
-  async createCategory(@UserId() userId: string) {
-    return `ok ${userId}`
+  async createCategory(
+    @Body() createCategory: CreateCategory,
+  ): Promise<CategoryEntity> {
+    return this.categoryService.createCategory(createCategory)
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { CreateCategory } from './dto/create-category.dto'
+import { CategoryEntity } from './entities/category.entity'
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createCategory(createCategory: CreateCategory): Promise<any> {
+    const { name } = createCategory
+    const categoryExists = await this.prisma.category
+      .findFirst({
+        where: {
+          name,
+        },
+      })
+      .catch(() => undefined)
+
+    if (categoryExists) {
+      throw new BadRequestException(
+        `Category name ${createCategory.name} exist`,
+      )
+    }
+
+    await this.prisma.category.create({
+      data: createCategory,
+    })
+  }
+}
