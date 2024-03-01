b4d71e70325d17328085e0a14f1f6ee5368a8e3b
lucasoliveiraa <lucasjackcosta18@gmail.com>
2024-02-29 23:35:36 -0300

    🔨 refactor: name

--- src/decorators/user-id.decorator.ts
+++ src/decorators/user-id.decorator.ts
@@ -1,10 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
-import { authorizantionToLoginPayload } from '../utils/base-64-converter'
+import { authorizationToLoginPayload } from '../utils/base-64-converter'
 
 export const UserId = createParamDecorator((_, ctx: ExecutionContext) => {
   const { authorization } = ctx.switchToHttp().getRequest().headers
 
-  const loginPayload = authorizantionToLoginPayload(authorization)
+  const loginPayload = authorizationToLoginPayload(authorization)
 
   return loginPayload?.id
 })
