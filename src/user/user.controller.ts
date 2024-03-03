53ef158909bf3736a6ade69c223b2842c574fd27
lucasoliveiraa <lucasjackcosta18@gmail.com>
2024-03-02 23:07:49 -0300

    🚧 chore: create admin and update password

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,49 @@
-import { Body, Controller, Post } from '@nestjs/common'
-import { CreateUserDto } from './dto/createUser.dto'
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common'
 import { UserService } from './user.service'
+import { ReturnUserDto } from './dto/return-user.dto'
+import { CreateUserDto } from './dto/create-user.dto'
+import { Roles } from 'src/decorators/roles.decorator'
+import { UserType } from './enum/user-type.enum'
+import { UserEntity } from './entities/user.entity'
+import { UpdatePassword } from './dto/update-user.dto'
+import { UserId } from 'src/decorators/user-id.decorator'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Roles(UserType.Root)
+  @Post('/admin')
+  async createAdmin(@Body() createUser: CreateUserDto): Promise<UserEntity> {
+    return this.userService.createUser(createUser, UserType.Admin)
+  }
+
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
+  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(createUser)
+  }
+
+  @Get('/all')
+  async getAllUsers(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAllUser()).map(
+      (userEntity) => new ReturnUserDto(userEntity),
+    )
+  }
+
+  // @Roles(UserType.Admin, UserType.Root)
+  // @Get('/:userId')
+  // async getUserById(@Param('userId') userId: number): Promise<ReturnUserDto> {
+  //   return new ReturnUserDto(
+  //     await this.userService.getUserByIdUsingRelations(userId),
+  //   )
+  // }
+
+  @Roles(UserType.Admin, UserType.Root, UserType.User)
+  @Patch('/update-password')
+  async updatePasswordUser(
+    @Body() updatePassword: UpdatePassword,
+    @UserId() userId: string,
+  ): Promise<UserEntity> {
+    return this.userService.updatePasswordUser(updatePassword, userId)
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,23 +1,29 @@
-import { ConflictException, Injectable } from '@nestjs/common'
-import { CreateUserDto } from './dto/createUser.dto'
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { UserType } from './enum/user-type.enum'
-import { createPasswordHashed } from 'src/utils/password'
+import { createPasswordHashed, validatePassword } from 'src/utils/password'
+import { UserEntity } from './entities/user.entity'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdatePassword } from './dto/update-user.dto'
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async createUser(createUserDto: CreateUserDto, userType?: number) {
+  async createUser(
+    createUserDto: CreateUserDto,
+    userType?: number,
+  ): Promise<UserEntity> {
     const { email, password } = createUserDto
 
-    const userWithSameEmail = await this.prisma.user
-      .findUnique({
-        where: {
-          email,
-        },
-      })
-      .catch(() => undefined)
+    const userWithSameEmail = await this.findUserByEmail(email).catch(
+      () => undefined,
+    )
 
     if (userWithSameEmail) {
       throw new ConflictException('Email registered in system')
@@ -25,7 +31,7 @@ export class UserService {
 
     const passwordHashed = await createPasswordHashed(password)
 
-    await this.prisma.user.create({
+    return this.prisma.user.create({
       data: {
         ...createUserDto,
         typeUser: userType || UserType.User,
@@ -34,7 +40,65 @@ export class UserService {
     })
   }
 
-  // async getAllUser(): Promise<UserEntity[]> {
-  //   return this.prisma.user.findMany()
-  // }
+  async getAllUser(): Promise<UserEntity[]> {
+    return this.prisma.user.findMany()
+  }
+
+  async findUserById(userId: string): Promise<UserEntity | null> {
+    const user = this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    })
+
+    if (!user) {
+      throw new NotFoundException(`UserId: ${userId} not found`)
+    }
+
+    return user
+  }
+
+  async findUserByEmail(email: string): Promise<UserEntity> {
+    const user = await this.prisma.user
+      .findUnique({
+        where: {
+          email,
+        },
+      })
+      .catch(() => undefined)
+
+    if (!user) {
+      throw new NotFoundException(`Email: ${email} not found`)
+    }
+
+    return user
+  }
+
+  async updatePasswordUser(
+    updatePassword: UpdatePassword,
+    userId: string,
+  ): Promise<UserEntity> {
+    const user = await this.findUserById(userId)
+
+    const passwordHash = await createPasswordHashed(updatePassword.newPassword)
+
+    const isMatch = await validatePassword(
+      updatePassword.lastPassword,
+      user?.password || '',
+    )
+
+    if (!isMatch) {
+      throw new BadRequestException('Last password invalid')
+    }
+
+    return this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...user,
+        password: passwordHash,
+      },
+    })
+  }
 }
