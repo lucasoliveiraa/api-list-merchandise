14dbb8d82e3cbe9a46db631547edea6a93dd7bbd
lucasoliveiraa <lucasjackcosta18@gmail.com>
2024-03-10 23:29:49 -0300

    🚧 chore: change validators dtos

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,21 +1,26 @@
-import { IsEmail, IsNumber, IsString } from 'class-validator'
+import { IsEmail, Max, IsPhoneNumber, IsString, Min } from 'class-validator'
 
 export class CreateUserDto {
   @IsString()
   name: string
 
-  @IsEmail()
+  @IsEmail(undefined, { message: 'E-mail inválido' })
   email: string
 
   @IsString()
   dateBirth: string
 
   @IsString()
+  @IsPhoneNumber('BR', { message: 'Telefone inválido' })
   phone: string
 
-  @IsString()
+  @IsString({ message: 'CPF inválido' })
+  @Max(11, { message: `deve ter no máximo 11 caracteres` })
+  @Min(11, { message: `deve ter no mínimo 11 caracteres` })
   cpf: string
 
   @IsString()
+  @Max(20, { message: `Senha deve ter no máximo 20 caracteres` })
+  @Min(6, { message: `Senha deve ter no mínimo 6 caracteres` })
   password: string
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,9 +1,11 @@
-import { IsString } from 'class-validator'
+import { IsString, Max, Min } from 'class-validator'
 
 export class UpdatePassword {
-  @IsString()
+  @IsString({ message: 'Senha atual inválido' })
+  @Max(20, { message: `Senha deve ter no máximo 20 caracteres` })
+  @Min(6, { message: `Senha deve ter no mínimo 6 caracteres` })
   newPassword: string
 
-  @IsString()
+  @IsString({ message: 'Senha anterior inválido' })
   lastPassword: string
 }
