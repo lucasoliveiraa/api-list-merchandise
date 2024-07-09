40b7b3d00d08162df22b97bb21cc161c48dad098
Lucas Oliveira <lucasjackcosta18@gmail.com>
2024-07-09 19:15:30 -0300

    🔧 Config swagger

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,33 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  const config = new DocumentBuilder()
+    .setTitle('List API Documentation')
+    .setDescription('API List Merchandise')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        description: `Insira apenas o token, sem aspas.`,
+        name: 'Authorization',
+        bearerFormat: 'Bearer',
+        scheme: 'Bearer',
+        type: 'http',
+        in: 'Header',
+      },
+      'access-token',
+    )
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger', app, document)
+
   const configService = app.get(ConfigService)
   const port = configService.get('PORT')
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
